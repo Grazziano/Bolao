02b21e908525f3204220247b5041c21615a5dadf
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-02-08 22:54:04 -0200

    Protegendo a página de usuários

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('admin')->namespace('Admin')->group(function () {
+Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function () {
     Route::resource('/users', 'UserController');
 });
