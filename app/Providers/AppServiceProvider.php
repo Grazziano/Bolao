f744004e017f1eb6a72fcf8cae89879c40107658
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-02-13 23:49:13 -0200

    Modelando o sistema de ACL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;   // Correção do erro (error SQLSTATE[42S01])
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,13 +15,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191); // Correção do erro (error SQLSTATE[42S01])
         Blade::component('components.alert', 'alert_component');
         Blade::component('components.breadcrumb', 'breadcrumb_component');
         Blade::component('components.search', 'search_component');
         Blade::component('components.table', 'table_component');
         Blade::component('components.paginate', 'paginate_component');
         Blade::component('components.page', 'page_component');
-        Blade::component('components.form', 'form_component');        
+        Blade::component('components.form', 'form_component');
     }
 
     /**
