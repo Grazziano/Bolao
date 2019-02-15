070c80181f67962538cf20e800fa1b0651b3a94c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-02-14 22:22:45 -0200

    Repositório para permissão

--- app/Http/Controllers/Admin/Permission.php
+++ app/Http/Controllers/Admin/Permission.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\Contracts\PermissionRepositoryInterface;
 use Validator;
 
 class PermissionController extends Controller
