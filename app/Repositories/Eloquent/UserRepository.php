34cc32b3bc40cc5b763360af395faa424bd1a307
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-02-16 23:17:30 -0300

    Corrigindo erro do CRUD de Usuários

--- app/Repositories/Eloquent/UserRepository.php
+++ app/Repositories/Eloquent/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Eloquent;
 
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 
 /**
  *
@@ -12,5 +13,24 @@ class UserRepository extends AbstractRepository implements UserRepositoryInterfa
 {
 
   protected $model = User::class;
+
+  public function create(array $data):Bool
+  {
+    $data['password'] = Hash::make($data['password']);
+    return (bool) $this->model->create($data);
+  }
+
+  public function update(array $data, int $id):Bool
+  {
+    $register = $this->find($id);
+    if ($register) {
+      if ($data['password'] ?? false) {
+        $data['password'] = Hash::make($data['password']);
+      }
+      return (bool) $register->update($data);
+    }else {
+      return false;
+    }
+  }
 
 }
