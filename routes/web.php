4882cfd73ab51609400d269ffc6643a22aee2bc5
Grazziano Fagundes <grazzianofagundes@gmail.com>
2019-02-17 22:23:17 -0300

    Utilizando o ACL na aplicação

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function () {
     Route::resource('/users', 'UserController');
+    Route::get('/users', 'UserController@index')->name('users.index')->middleware('can:list-users');
+    Route::get('/users/create', 'UserController@create')->name('users.create')->middleware('can:create-users');
+    Route::post('/users', 'UserController@store')->name('users.store')->middleware('can:create-users');
     Route::resource('/permissions', 'PermissionController');
     Route::resource('/roles', 'RoleController');
 });
