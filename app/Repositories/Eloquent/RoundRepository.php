7f65b96426cf2fe2821f9859f2a6a6c88d2b1af0
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-02 22:34:38 -0300

    Repositório de rodadas

---
+++ app/Repositories/Eloquent/RoundRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Contracts\RoundRepositoryInterface;
+use App\Round;
+
+/**
+ *
+ */
+class RoundRepository extends AbstractRepository implements RoundRepositoryInterface
+{
+
+  protected $model = Round::class;
+
+}
