4eb1c426dc376e553c6f51c3cf6ecb8ae3e6c5a2
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-02 22:58:04 -0300

    Formulário de rodadas - Parte 1

--- resources/views/admin/rounds/form.blade.php
+++ resources/views/admin/rounds/form.blade.php
@@ -1,19 +1,30 @@
 <div class="row">
   <div class="form-group col-6">
-    <label for="name">{{__('bolao.name')}}</label>
-    <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') ?? ($register->name ?? '') }}">
-    @if ($errors->has('name'))
+    <label for="title">{{__('bolao.title')}}</label>
+    <input type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title') ?? ($register->title ?? '') }}">
+    @if ($errors->has('title'))
         <span class="invalid-feedback" role="alert">
-            <strong>{{ $errors->first('name') }}</strong>
+            <strong>{{ $errors->first('title') }}</strong>
         </span>
     @endif
   </div>
+
   <div class="form-group col-6">
-    <label for="description">{{__('bolao.description')}}</label>
-    <input type="text" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description" value="{{ old('description') ?? ($register->description ?? '') }}">
-    @if ($errors->has('description'))
+    <label for="date_start">{{__('bolao.date_start')}} ({{ date('d-m-Y H:i:s') }})</label>
+    <input type="datetime" placeholder="{{ date('d-m-Y H:i:s') }}" class="form-control{{ $errors->has('date_start') ? ' is-invalid' : '' }}" name="date_start" value="{{ old('date_start') ?? ($register->date_start ?? '') }}">
+    @if ($errors->has('date_start'))
         <span class="invalid-feedback" role="alert">
-            <strong>{{ $errors->first('description') }}</strong>
+            <strong>{{ $errors->first('date_start') }}</strong>
+        </span>
+    @endif
+  </div>
+
+  <div class="form-group col-6">
+    <label for="date_end">{{__('bolao.date_end')}} ({{ date('d-m-Y H:i:s') }})</label>
+    <input type="datetime" placeholder="{{ date('d-m-Y H:i:s') }}" class="form-control{{ $errors->has('date_end') ? ' is-invalid' : '' }}" name="date_end" value="{{ old('date_end') ?? ($register->date_end ?? '') }}">
+    @if ($errors->has('date_end'))
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $errors->first('date_end') }}</strong>
         </span>
     @endif
   </div>
