cdb67afe6cf2fc86c4cca28f16d97880dae40f6c
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-02 22:11:20 -0300

    Modelo e migração de rodadas

--- app/Betting.php
+++ app/Betting.php
@@ -6,32 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Betting extends Model
 {
-  protected $fillable = [
-      'user_id',
-      'title',
-      'current_round', // vai incrementar a cada rodada
-      'value_result',
-      'extra_value',
-      'value_fee',
-  ];
+    protected $fillable = [
+        'user_id',
+        'title',
+        'current_round', // vai incrementar a cada rodada
+        'value_result',
+        'extra_value',
+        'value_fee',
+    ];
 
-  public function user()
-  {
-    return $this->belongsTo('App\User');
-  }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 
-  public function getFirstNameAttribute($value)
-  {
-    return ucfirst($value);
-  }
+    public function getFirstNameAttribute($value)
+    {
+        return ucfirst($value);
+    }
 
-  public function getTitleAttribute($value)
-  {
-    return ucfirst($value);
-  }
+    public function getTitleAttribute($value)
+    {
+        return ucfirst($value);
+    }
 
-  public function getUserNameAttribute()
-  {
-    return $this->user->name;
-  }
+    public function getUserNameAttribute()
+    {
+        return $this->user->name;
+    }
+
+    public function rounds()
+    {
+        return $this->hasMany('App\Round');
+    }
 }
