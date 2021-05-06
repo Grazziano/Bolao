332789c829932846c9930f06da10aa1e59a98ecd
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-06 14:43:37 -0300

    Botões na tela principal

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -37,6 +37,9 @@
                         <div class="portfolio-caption">
                             <h4>{{ $value->title }}</h4>
                             <p class="text-muted">{{ $value->user_name }}</p>
+                            <a class="btn btn-info" href="">Ver Rodadas</a>
+                            <button class="btn btn-danger">Deixar Bolão</button>
+                            <button class="btn btn-success">Participar</button>
                         </div>
                     </div>
                 @endforeach
@@ -115,10 +118,49 @@
         </div>
     </section>
 
+    @foreach ($list as $key => $value)
+        <div class="portfolio-modal modal fade" id="portfolioModal{{ $value->id }}" tabindex="-1" role="dialog"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="close-modal" data-dismiss="modal">
+                        <div class="lr">
+                            <div class="rl"></div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <div class="modal-body">
+                                    <!-- Project Details Go Here -->
+                                    <h2 class="text-uppercase">{{ $value->title }}</h2>
+                                    <p class="item-intro text-muted">{{ $value->user_name }}</p>
+                                    {{-- <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt=""> --}}
+                                    <p>Este bolão tem as seguintes regras:</p>
+                                    <ul class="list-inline">
+                                        <li>Valor do Resultado: {{ $value->value_result }}</li>
+                                        <li>Valor Extra: {{ $value->extra_value }}</li>
+                                        <li>Taxa: {{ $value->value_fee }}</li>
+                                    </ul>
+                                    <a class="btn btn-info" href="">Ver Rodadas</a>
+                                    <button class="btn btn-danger">Deixar Bolão</button>
+                                    <button class="btn btn-success">Participar</button>
+                                    <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                        <i class="fas fa-times"></i>
+                                        Close Project</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
     <!-- Portfolio Modals -->
 
     <!-- Modal 1 -->
-    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+    {{-- <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="close-modal" data-dismiss="modal">
@@ -154,7 +196,7 @@
         </div>
     </div>
 
-    {{-- <!-- Modal 2 -->
+    <!-- Modal 2 -->
     <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
